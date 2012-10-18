81f04b88379ac41b77cc6b59b78d66b0092e0bdb
SmokeyMcBong <blackthesoul@gmail.com>
2012-10-18 22:45:32 +0100

    initial build commit ....
    Layout Partially Configured, configured App for Android 4.1+, removed
    hardware 'menu' key activity, added direct quit button,  also added
    short simple 'about' toast message, configured initial app icon,
    configured initial manifest

---
+++ ReadME.txt
@@ -0,0 +1,33 @@
+
+
+--------------------------------------------------------------------------------------------------------------------------
+                                #######   New_Baseline_App [McBong-Utility] []   #######
+--------------------------------------------------------------------------------------------------------------------------
+
+					 This App is a MAJOR 'Work-In-Progress' ! 
+			[and will be updated and reconfigured hundreds more times before any kind of release]
+
+--------------------------------------------------------------------------------------------------------------------------
+
+
+
+Update 18/10/2012 ...   Initial Git commit.....
+
+				> Layout Partially Configured, configured App for Android 4.1+, removed hardware 'menu' key activity, added direct quit button, 						  	also added short simple 'about' toast message, configured initial app icon, configured initial manifest. 							
+			
+
+
+
+
+
+						
+
+
+
+
+
+
+
+
+
+                                              \\\.\\\\. BREAK .///.////
---
+++ src/com/mcbong/utility/StartActivity.java
@@ -0,0 +1,112 @@
+package com.mcbong.utility;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+//import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+//import android.widget.Toast;
+
+public class StartActivity extends Activity {
+	public static Context appContext;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        appContext = getApplicationContext();
+
+       //ActionBar
+        ActionBar actionbar = getActionBar();
+        actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        ActionBar.Tab TweakTab = actionbar.newTab().setText("Tool's & Shortcut's..");
+        ActionBar.Tab AddonTab = actionbar.newTab().setText("Tweak's & Hack's..");
+        ActionBar.Tab OnlineTab = actionbar.newTab().setText("Online..");
+        ActionBar.Tab RecoveryTab = actionbar.newTab().setText("Recovery..");
+        
+        Fragment TweakFragment = new Tab1();
+        Fragment AddonFragment = new Tab2();
+        Fragment OnlineFragment = new Tab3();
+        Fragment RecoveryFragment = new Tab4();
+
+        TweakTab.setTabListener(new MyTabsListener(TweakFragment));
+        AddonTab.setTabListener(new MyTabsListener(AddonFragment));
+        OnlineTab.setTabListener(new MyTabsListener(OnlineFragment));
+        RecoveryTab.setTabListener(new MyTabsListener(RecoveryFragment));
+
+        actionbar.addTab(TweakTab);
+        actionbar.addTab(AddonTab);
+        actionbar.addTab(OnlineTab);
+        actionbar.addTab(RecoveryTab);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+  //Set Back Key .....
+  	@Override
+  	public void onBackPressed() {
+
+  	   return;
+  	}
+  //.... /Set Back Key*/
+    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			//case R.id.menuitem_about:
+			//	Toast.makeText(appContext, "'McBong-Utility' } Internal test build -v1.0", Toast.LENGTH_SHORT).show();
+			//	return true;
+			case R.id.menuitem_quit:
+				//Toast.makeText(appContext, "quit", Toast.LENGTH_SHORT).show();
+				android.os.Process.killProcess(android.os.Process.myPid());
+				return true;
+		}
+		return false;
+	}
+	
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+    }
+    
+}
+
+
+
+class MyTabsListener implements ActionBar.TabListener {
+	public Fragment fragment;
+	
+	public MyTabsListener(Fragment fragment) {
+		this.fragment = fragment;
+	}
+	
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		//Toast.makeText(StartActivity.appContext, "This Tab is already in view", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		ft.replace(R.id.fragment_container, fragment);
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		ft.remove(fragment);
+	}
+	
+}
